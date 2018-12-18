f6dccdedca576f70577ef5d63afaf9bae026e743
MOAAS <38762849+MOAAS@users.noreply.github.com>
2018-12-18 19:20:25 +0000

    Update proj.c

--- proj/proj.c
+++ proj/proj.c
@@ -19,6 +19,7 @@
 #include "bitmap.h"
 #include "canvas.h"
 #include "layer.h"
+#include "uart.h"
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -26,7 +27,7 @@ int main(int argc, char *argv[]) {
 
   // enables to log function invocations that are being "wrapped" by LCF
   // [comment this out if you don't want/need it]
-  //lcf_trace_calls("/home/lcom/labs/proj/trace.txt");
+ // lcf_trace_calls("/home/lcom/labs/proj/trace.txt");
 
   // enables to save the output of printf function calls on a file
   // [comment this out if you don't want/need it]
@@ -44,9 +45,71 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-
 int (proj_main_loop)() { //int argc, char *argv[]) {
   projeto();
+  return 0;
+  subscribe_device(SerialPort);
+  subscribe_device(Keyboard);
+  subscribe_device(Mouse);
+  while (1) {
+    Event_t event = GetEvent();
+    if (event.isMouseEvent && event.mouseEvent.type == LB_PRESS) {
+      printf("SENDING!\n");
+      uart_send_draw_line1(364, 364, 364, 364, 16711680, 4);
+      uart_send_draw_line1(364, 364, 368, 368, 16711680, 4);
+     // uart_fifo_send(notif.mouse_packet.delta_x);
+     // uart_fifo_send(notif.mouse_packet.delta_y);
+     // uart_fifo_send(notif.mouse_packet.lb);
+     // uart_fifo_send(notif.mouse_packet.rb);
+     // uart_fifo_send(notif.mouse_packet.mb);
+     // uart_fifo_send(notif.mouse_packet.delta_x);
+     // uart_fifo_send(notif.mouse_packet.delta_y);
+     // uart_fifo_send(notif.mouse_packet.lb);
+     // uart_fifo_send(notif.mouse_packet.rb);
+     // uart_fifo_send(notif.mouse_packet.mb);
+     // uart_fifo_send(notif.mouse_packet.delta_x);
+     // uart_fifo_send(notif.mouse_packet.delta_y);
+     // uart_fifo_send(notif.mouse_packet.lb);
+     // uart_fifo_send(notif.mouse_packet.rb);
+     // uart_fifo_send(notif.mouse_packet.mb);
+     // uart_fifo_send(notif.mouse_packet.delta_x);
+     // uart_fifo_send(notif.mouse_packet.delta_y);
+     // uart_fifo_send(notif.mouse_packet.lb);
+     // uart_fifo_send(notif.mouse_packet.rb);
+     // uart_fifo_send(notif.mouse_packet.mb);
+     // uart_fifo_send(notif.mouse_packet.delta_x);
+     // uart_fifo_send(notif.mouse_packet.delta_y);
+     // uart_fifo_send(notif.mouse_packet.lb);
+     // uart_fifo_send(notif.mouse_packet.rb);
+     // uart_fifo_send(notif.mouse_packet.mb);
+     // uart_fifo_send(notif.mouse_packet.delta_x);
+     // uart_fifo_send(notif.mouse_packet.delta_y);
+     // uart_fifo_send(notif.mouse_packet.lb);
+     // uart_fifo_send(notif.mouse_packet.rb);
+     // uart_fifo_send(notif.mouse_packet.mb);
+     // uart_fifo_send(notif.mouse_packet.delta_x);
+     // uart_fifo_send(notif.mouse_packet.delta_y);
+     // uart_fifo_send(notif.mouse_packet.lb);
+     // uart_fifo_send(notif.mouse_packet.rb);
+     // uart_fifo_send(notif.mouse_packet.mb);
+     // uart_fifo_send(notif.mouse_packet.delta_x);
+     // uart_fifo_send(notif.mouse_packet.delta_y);
+     // uart_fifo_send(notif.mouse_packet.lb);
+     // uart_fifo_send(notif.mouse_packet.rb);
+     // uart_fifo_send(notif.mouse_packet.mb);
+    }
+    if (event.isKeyboardEvent && event.keyboardEvent.type == ESC_PRESS)
+      break;
+    if (event.isUARTEvent) {
+      printf("num uart_messages: %d\n", event.num_uart_messages);
+      for (int j = 0; j < event.num_uart_messages; j++) {
+        uart_receive_draw_line1(&event.uart_messages[j]);
+      }
+    }
+  }
+  unsubscribe_device(Mouse);
+  unsubscribe_device(Keyboard);
+  unsubscribe_device(SerialPort);
   return 0;
 }
 
